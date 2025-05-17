282005f06a7256966b0f78c8d10f7fe2a8b8e721
pnavlani <p.navlani@sapalomera.cat>
2025-05-17 22:59:02 +0200

    Staking Period

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -5,15 +5,38 @@ pragma solidity 0.8.24;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+// Staking fixed amount
+
 
 contract StakingApp is Ownable{
 
     // Variables
     address public stakingToken;
+    uint256 public stakingPeriod;
+
+    event ChangeStakingPeriod(uint256 newStakingPeriod_);
     
     
-    constructor(address stakingToken_ ,  address owner_)  Ownable(owner_){
+    constructor(address stakingToken_ ,  address owner_, uint256 stakingPeriod_)  Ownable(owner_){
         stakingToken = stakingToken_;
+        stakingPeriod = stakingPeriod_;
 
     }
+
+    // Functions 
+
+    // External functions 
+
+    // Internal functions 
+
+    // Staking reward period
+    function changeStakingPeriod(uint256 newStakingPeriod_) external onlyOwner {
+        stakingPeriod = newStakingPeriod_;
+        emit ChangeStakingPeriod(newStakingPeriod_);
+    }
+
+    
+    
+
+
 }
